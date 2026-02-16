2888b55d978bfb6e71b5a71a2f186027e97723a4
Davis Ejiro Owvwiomodiowho <Davisejiro2000@gmail.com>
2026-02-16 12:19:05 +0100

    Delete OverdraftAttemptRepository.java

--- OverdraftAttemptRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apex.bank.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.apex.bank.model.OverdraftAttempt;
-
-public interface OverdraftAttemptRepository
-        extends JpaRepository<OverdraftAttempt, Long> {
-}
