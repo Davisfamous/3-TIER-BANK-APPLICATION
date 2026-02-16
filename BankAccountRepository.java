92f814e669d2ce92467bf29648d9dd80a4a408cc
Davis Ejiro Owvwiomodiowho <Davisejiro2000@gmail.com>
2026-02-16 11:51:22 +0100

    Delete BankAccountRepository.java

--- BankAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apex.bank.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.apex.bank.model.BankAccount;
-
-public interface BankAccountRepository
-        extends JpaRepository<BankAccount, String> {
-}
